25847d187b1d743e375ede85f68f4048fc99a95d
Carlos Gartner <carlos@trezo.com.br>
2016-10-27 15:41:33 -0200

    Change user chown

--- dipirona.sh
+++ dipirona.sh
@@ -6,7 +6,7 @@ find . -type d -exec chmod 755 {} \;
 find ./var -type d -exec chmod 777 {} \;
 find ./pub/media -type d -exec chmod 777 {} \;
 find ./pub/static -type d -exec chmod 777 {} \;
-chown -R carlos:carlos *;
+chown -R carlos:carlos ./;
 chmod -R 777 app/etc;
 chmod -R 644 app/etc/*.xml;
 chmod u+x bin/magento;
